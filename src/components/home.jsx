dab0c8b6dd037c7cf9e6c8a2e8e60012c70485e1
Nicolas Suarez <2jmk3218@gmail.com>
2023-05-15 23:32:18 -0500

    lab  11

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,7 @@ import background from '../images/background.jpeg'
 
 export class Home extends Component {
   render() {
-    let aux = dataBase.slice(6, 9);
+    let aux = dataBase.slice(8, 11);
     return (
             <div>
                 <Navbar labs={dataBase}/>
